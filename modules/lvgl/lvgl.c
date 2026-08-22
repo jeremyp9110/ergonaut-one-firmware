721fd3c392d145a5631143d9085e969dde9b8122
jeremyp9110 <184771436+jeremyp9110@users.noreply.github.com>
2026-08-22 21:46:05 +0200

    Include stdlib.h in lvgl.c

--- modules/lvgl/lvgl.c
+++ modules/lvgl/lvgl.c
@@ -15,7 +15,7 @@
 #ifdef CONFIG_LV_Z_MEM_POOL_SYS_HEAP
 #include "lvgl_mem.h"
 #endif
-#include LV_MEM_CUSTOM_INCLUDE
+#include <stdlib.h>
 
 #define LOG_LEVEL CONFIG_LV_LOG_LEVEL
 #include <zephyr/logging/log.h>
