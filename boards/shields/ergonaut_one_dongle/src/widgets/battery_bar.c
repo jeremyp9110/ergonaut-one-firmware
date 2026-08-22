c878a558dce38496909096fab4d4961a67e63819
jeremyp9110 <184771436+jeremyp9110@users.noreply.github.com>
2026-08-22 21:08:00 +0200

    Remove connection update handling from battery bar
    Removed connection update handling and related structures to simplify battery bar widget functionality.

--- boards/shields/ergonaut_one_dongle/src/widgets/battery_bar.c
+++ boards/shields/ergonaut_one_dongle/src/widgets/battery_bar.c
@@ -5,7 +5,6 @@
 #include <zmk/ble.h>
 #include <zmk/events/battery_state_changed.h>
 #include <zmk/event_manager.h>
-#include <zmk/events/split_central_status_changed.h>
 #include <fonts.h>
 
 #include <zephyr/logging/log.h>
@@ -15,15 +14,9 @@ static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
 bool initialized = false;
 
-// Simplified state structures for each event type
 struct battery_update_state {
     uint8_t source;
     uint8_t level;
-};
-
-struct connection_update_state {
-    uint8_t source;
-    bool connected;
 };
 
 static void set_battery_bar_value(lv_obj_t *widget, struct battery_update_state state) {
@@ -49,32 +42,6 @@ static void set_battery_bar_value(lv_obj_t *widget, struct battery_update_state 
     }
 }
 
-static void set_battery_bar_connected(lv_obj_t *widget, struct connection_update_state state) {
-    if (initialized) {
-        lv_obj_t *info_container = lv_obj_get_child(widget, state.source);
-        lv_obj_t *bar = lv_obj_get_child(info_container, 0);
-        lv_obj_t *num = lv_obj_get_child(info_container, 1);
-        lv_obj_t *nc_bar = lv_obj_get_child(info_container, 2);
-        lv_obj_t *nc_num = lv_obj_get_child(info_container, 3);
-
-        LOG_DBG("Peripheral %d %s", state.source,
-                state.connected ? "connected" : "disconnected");
-
-        if (state.connected) {
-            lv_obj_fade_out(nc_bar, 150, 0);
-            lv_obj_fade_out(nc_num, 150, 0);
-            lv_obj_fade_in(bar, 150, 250);
-            lv_obj_fade_in(num, 150, 250);
-        } else {
-            lv_obj_fade_out(bar, 150, 0);
-            lv_obj_fade_out(num, 150, 0);
-            lv_obj_fade_in(nc_bar, 150, 250);
-            lv_obj_fade_in(nc_num, 150, 250);
-        }
-    }
-}
-
-// Battery event handling
 void battery_bar_battery_update_cb(struct battery_update_state state) {
     LOG_DBG("Battery update: source=%d, level=%d", state.source, state.level);
 
@@ -96,53 +63,22 @@ static struct battery_update_state battery_bar_get_battery_state(const zmk_event
     };
 }
 
-// Connection event handling
-void battery_bar_connection_update_cb(struct connection_update_state state) {
-    LOG_DBG("Connection update: source=%d, connected=%s", state.source, state.connected ? "true" : "false");
-
-    struct zmk_widget_battery_bar *widget;
-    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) {
-        set_battery_bar_connected(widget->obj, state);
-    }
-}
-
-static struct connection_update_state battery_bar_get_connection_state(const zmk_event_t *eh) {
-    const struct zmk_split_central_status_changed *conn_ev =
-        as_zmk_split_central_status_changed(eh);
-
-    LOG_DBG("Received connection event: slot=%d, connected=%s", conn_ev->slot, conn_ev->connected ? "true" : "false");
-
-    return (struct connection_update_state){
-        .source = conn_ev->slot,
-        .connected = conn_ev->connected,
-    };
-}
-
-// Separate widget listeners for each event type
 ZMK_DISPLAY_WIDGET_LISTENER(widget_battery_bar_battery, struct battery_update_state,
                             battery_bar_battery_update_cb, battery_bar_get_battery_state);
 ZMK_SUBSCRIPTION(widget_battery_bar_battery, zmk_peripheral_battery_state_changed);
-
-ZMK_DISPLAY_WIDGET_LISTENER(widget_battery_bar_connection, struct connection_update_state,
-                            battery_bar_connection_update_cb, battery_bar_get_connection_state);
-ZMK_SUBSCRIPTION(widget_battery_bar_connection, zmk_split_central_status_changed);
 
 int zmk_widget_battery_bar_init(struct zmk_widget_battery_bar *widget, lv_obj_t *parent) {
     widget->obj = lv_obj_create(parent);
     lv_obj_set_width(widget->obj, lv_pct(100));
     lv_obj_set_flex_flow(widget->obj, LV_FLEX_FLOW_ROW);
     lv_obj_set_flex_align(widget->obj,
-                          LV_FLEX_ALIGN_SPACE_EVENLY, // Main axis (horizontal)
-                          LV_FLEX_ALIGN_CENTER,       // Cross axis (vertical)
-                          LV_FLEX_ALIGN_CENTER        // Track alignment
+                          LV_FLEX_ALIGN_SPACE_EVENLY,
+                          LV_FLEX_ALIGN_CENTER,
+                          LV_FLEX_ALIGN_CENTER
     );
     lv_obj_set_style_pad_column(widget->obj, 12, LV_PART_MAIN);
-    // lv_obj_set_style_pad_all(widget->obj, 8, LV_PART_MAIN);
     lv_obj_set_style_pad_bottom(widget->obj, 12, LV_PART_MAIN);
-    // lv_obj_set_style_pad_top(widget->obj, 30, LV_PART_MAIN);
     lv_obj_set_style_pad_hor(widget->obj, 16, LV_PART_MAIN);
-
-    // lv_obj_add_flag(widget->obj, LV_OBJ_FLAG_OVERFLOW_VISIBLE);
 
     for (int i = 0; i < ZMK_SPLIT_BLE_PERIPHERAL_COUNT; i++) {
         lv_obj_t *info_container = lv_obj_create(widget->obj);
@@ -159,7 +95,6 @@ int zmk_widget_battery_bar_init(struct zmk_widget_battery_bar *widget, lv_obj_t 
         lv_obj_set_style_bg_color(bar, lv_color_hex(0x909090), LV_PART_INDICATOR);
         lv_obj_set_style_bg_opa(bar, 255, LV_PART_INDICATOR);
         lv_obj_set_style_bg_grad_color(bar, lv_color_hex(0xf0f0f0), LV_PART_INDICATOR);
-        lv_obj_set_style_bg_dither_mode(bar, LV_DITHER_ERR_DIFF, LV_PART_INDICATOR);
         lv_obj_set_style_bg_grad_dir(bar, LV_GRAD_DIR_HOR, LV_PART_INDICATOR);
         lv_obj_set_style_radius(bar, 1, LV_PART_INDICATOR);
         lv_obj_set_style_anim_time(bar, 250, 0);
@@ -194,10 +129,11 @@ int zmk_widget_battery_bar_init(struct zmk_widget_battery_bar *widget, lv_obj_t 
     sys_slist_append(&widgets, &widget->node);
 
     widget_battery_bar_battery_init();
-    widget_battery_bar_connection_init();
     initialized = true;
 
     return 0;
 }
 
-lv_obj_t *zmk_widget_battery_bar_obj(struct zmk_widget_battery_bar *widget) { return widget->obj; }
+lv_obj_t *zmk_widget_battery_bar_obj(struct zmk_widget_battery_bar *widget) { 
+    return widget->obj; 
+}
